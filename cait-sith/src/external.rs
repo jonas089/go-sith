26dc9bf976a06c656205a45f9a0d8b995295bffd
jonas pauli <jonaspauli089@gmail.com>
2025-02-07 13:48:22 +0100

    simplify keygen

--- cait-sith/src/external.rs
+++ cait-sith/src/external.rs
@@ -1,32 +1,15 @@
 use crate::{
     keygen,
     protocol::{run_protocol, Participant, Protocol},
-    triples::{self, deal, TriplePub, TripleShare},
+    triples::{deal, TriplePub, TripleShare},
     KeygenOutput,
 };
 use k256::Secp256k1;
 use rand_core::OsRng;
-use std::os::raw::c_uint;
-use std::{
-    collections::HashMap,
-    ffi::{c_char, CString},
-};
-
-#[repr(C)]
-pub struct KeygenResult {
-    participants: *mut c_uint,     // Pointer to array of int
-    keygen_outs: *mut *mut c_char, // Pointer to array of C strings
-    length: usize,
-}
-
-#[repr(C)]
-pub struct TripleResult {
-    triples: *mut c_char,
-    other_triples: *mut c_char,
-}
+use std::ffi::c_char;
 
 #[no_mangle]
-pub extern "C" fn ext_generate_keys(parties: u32, threshold: u32) -> KeygenResult {
+pub extern "C" fn ext_generate_keys(parties: u32, threshold: u32) -> *mut c_char {
     let participants: Vec<Participant> =
         (0..parties).map(|p| Participant::from(p as u32)).collect();
     let mut protocols: Vec<(
@@ -40,35 +23,9 @@ pub extern "C" fn ext_generate_keys(parties: u32, threshold: u32) -> KeygenResul
         protocols.push((*p, Box::new(protocol)));
     }
     let keygen_out: Vec<(Participant, KeygenOutput<Secp256k1>)> = run_protocol(protocols).unwrap();
-    let mut ordered_mapping: Vec<(u32, String)> = keygen_out
-        .iter()
-        .map(|(p, keygen_out)| {
-            let id = u32::from(*p);
-            let json = serde_json::to_string(keygen_out).unwrap();
-            (id, json)
-        })
-        .collect();
-    ordered_mapping.sort_by_key(|&(id, _)| id);
-    let participants_u32: Vec<u32> = ordered_mapping.iter().map(|&(id, _)| id).collect();
-    let keygen_outs_serialized: Vec<CString> = ordered_mapping
-        .iter()
-        .map(|(_, json)| CString::new(json.clone()).unwrap())
-        .collect();
-    let keygen_outs_ptrs: Vec<*mut c_char> = keygen_outs_serialized
-        .iter()
-        .map(|s| s.clone().into_raw())
-        .collect();
-    let participants_ptr = Box::into_raw(participants_u32.into_boxed_slice()) as *mut c_uint;
-    let keygen_outs_ptr = Box::into_raw(keygen_outs_ptrs.into_boxed_slice()) as *mut *mut c_char;
-    assert_eq!(
-        keygen_out.first().unwrap().1.public_key,
-        keygen_out.get(1).unwrap().1.public_key
-    );
-    KeygenResult {
-        participants: participants_ptr,
-        keygen_outs: keygen_outs_ptr,
-        length: ordered_mapping.len(),
-    }
+    let keygen_out_serialized = serde_json::to_string(&keygen_out).unwrap();
+    let keygen_out_ptr = Box::into_raw(keygen_out_serialized.into_boxed_str()) as *mut c_char;
+    keygen_out_ptr
 }
 
 #[no_mangle]
--- main.go
+++ main.go
@@ -13,64 +13,31 @@ package main
 #include <stdint.h>
 #include <stdlib.h>
 
-typedef struct {
-    uint32_t *participants;
-    char **keygen_out;
-    size_t length;
-} KeygenResult;
-
-extern KeygenResult ext_generate_keys(uint32_t parties, uint32_t threshold);
+extern char* ext_generate_keys(uint32_t parties, uint32_t threshold);
 extern char* ext_deal_triples(size_t parties, size_t threshold);
 */
 import "C"
 
 import (
 	"fmt"
-	"unsafe"
 )
 
 func main() {
+	example_keygen_participants()
 	example_deal_triples()
 }
 
-func example_keygen_participants() []*C.char {
+func example_keygen_participants() C.char {
 	numParticipants := C.uint32_t(2)
 	numThreshold := C.uint32_t(2)
-	result := C.ext_generate_keys(numParticipants, numThreshold)
-	if result.participants == nil || result.keygen_out == nil || result.length == 0 {
-		fmt.Println("Failed to generate key keygen_out")
-		return nil
-	}
-	// same for keygen_out
-	keysPtr := (*[1 << 18]*C.char)(unsafe.Pointer(result.keygen_out))[:result.length:result.length] // Convert **C.char to slice
-	/*for i := 0; i < int(result.length); i++ {
-		if keygen_outPtr[i] == nil {
-			//fmt.Printf("Warning: NULL string at index %d\n", i)
-			keygen_out[i] = "(NULL)"
-		} else {
-			keygen_out[i] = C.GoString(keygen_outPtr[i]) // Convert C string to Go string
-		}
-	}*/
-	//fmt.Println("Final Participants [Go]:", participants)
-	//fmt.Println("keygen_out [Go]:")
-	/*for i, share := range keygen_out {
-		fmt.Printf("Participant %d: %s\n", participants[i], share)
-	}*/
-	return keysPtr
+	serialized_keygen_out := C.ext_generate_keys(numParticipants, numThreshold)
+	fmt.Println("Keys:", serialized_keygen_out)
+	return *serialized_keygen_out
 }
 
 func example_deal_triples() C.char {
-	// participants should be from keygen
-	// results should be from keygen
-	//keys := example_keygen_participants()
-	//numResults := C.size_t(len(keys))
 	numParticipants := C.size_t(2)
-	//resultsPtr := (**C.char)(unsafe.Pointer(&keys[0]))
-	// Call the C function
 	serialized_triples := C.ext_deal_triples(numParticipants, 2)
-	// Convert C **char to Go slice of strings (triples)
-	// triplesJSON := C.GoString((*C.char)(unsafe.Pointer(result.triples)))
-	// otherTriplesJSON := C.GoString((*C.char)(unsafe.Pointer(result.other_triples)))
 	fmt.Println("Triples:", serialized_triples)
 	return *serialized_triples
 }
